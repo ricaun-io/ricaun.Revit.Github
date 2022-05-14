9dc7793d32c9344c328825ad8ed65d902084b63a
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-05-14 10:31:37 -0300

    Add IRevitPackageBuilder

--- Build/Build.cs
+++ Build/Build.cs
@@ -4,9 +4,9 @@ using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
 [CheckBuildProjectConfigurations]
-class Build : NukeBuild, IPublishPack//, IRevitPackageBuilder
+class Build : NukeBuild, IPublishPack, IRevitPackageBuilder
 {
-    //    string IHazPackageBuilderProject.Name => "Example";
-    //    string IHazRevitPackageBuilder.Application => "Revit.App";
+    string IHazPackageBuilderProject.Name => "Example";
+    string IHazRevitPackageBuilder.Application => "Revit.App";
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
 }
