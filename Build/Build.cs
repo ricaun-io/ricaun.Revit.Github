00a5aa901d29790d890fd9aba22a41968929ad9a
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-06-17 14:23:26 -0300

    - Update net6.0

--- Build/Build.cs
+++ Build/Build.cs
@@ -3,7 +3,6 @@ using Nuke.Common.Execution;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
-[CheckBuildProjectConfigurations]
 class Build : NukeBuild, IPublishPack, IRevitPackageBuilder
 {
     string IHazPackageBuilderProject.Name => "Example";
